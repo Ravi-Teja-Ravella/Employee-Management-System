0efc0bfca77b4ce79747152c1e15a86d0e540850
teja00 <tejanagubandi00@gmail.com>
2024-12-02 21:44:07 -0600

    optimizing the base code

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,18 +1,50 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
+# from wtforms_sqlalchemy.fields import QuerySelectField
+# from ..models import Department, Role, Grade
+
+# class DepartmentForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired()])
+#     description = StringField('Description', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
+
+# class RoleForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired()])
+#     description = StringField('Description', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
+
+# class GradeForm(FlaskForm):
+#     paygrade = StringField('Pay Grade', validators=[DataRequired()])
+#     amount = StringField('Amount', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
+
+# class EmployeeAssignForm(FlaskForm):
+#     department = QuerySelectField(query_factory=lambda: Department.query.all(), get_label="name")
+#     role = QuerySelectField(query_factory=lambda: Role.query.all(), get_label="name")
+#     grade = QuerySelectField(query_factory=lambda: Grade.query.all(), get_label="paygrade")
+#     submit = SubmitField('Submit')
+
+
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms_sqlalchemy.fields import QuerySelectField
 from ..models import Department, Role, Grade
 
-class DepartmentForm(FlaskForm):
+# Common Base Form
+class BaseForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-class RoleForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+# Specific Forms
+class DepartmentForm(BaseForm):
+    pass
+
+class RoleForm(BaseForm):
+    pass
 
 class GradeForm(FlaskForm):
     paygrade = StringField('Pay Grade', validators=[DataRequired()])
@@ -20,7 +52,7 @@ class GradeForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class EmployeeAssignForm(FlaskForm):
-    department = QuerySelectField(query_factory=lambda: Department.query.all(), get_label="name")
-    role = QuerySelectField(query_factory=lambda: Role.query.all(), get_label="name")
-    grade = QuerySelectField(query_factory=lambda: Grade.query.all(), get_label="paygrade")
+    department = QuerySelectField(query_factory=lambda: Department.query.all(), get_label="name", allow_blank=True)
+    role = QuerySelectField(query_factory=lambda: Role.query.all(), get_label="name", allow_blank=True)
+    grade = QuerySelectField(query_factory=lambda: Grade.query.all(), get_label="paygrade", allow_blank=True)
     submit = SubmitField('Submit')
